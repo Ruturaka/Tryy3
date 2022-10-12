9ade7786171a438112d4ffe4316e5ef13732ef8c
Ruturaka2700 <ruturaka2700@gmail.com>
2022-10-12 11:09:16 +0530

    initial commit

---
+++ src/main/java/com/codewithrutu/demo/repository/TutorialRepository.java
@@ -0,0 +1,24 @@
+package com.codewithrutu.demo.repository;
+
+import com.codewithrutu.demo.model.Tutorial;
+
+import java.util.List;
+
+public interface TutorialRepository {
+
+    int save(Tutorial book);
+
+    int update(Tutorial book);
+
+    Tutorial findById(Long id);
+
+    int deleteById(Long id);
+
+    List<Tutorial> findAll();
+
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+
+    int deleteAll();
+}
